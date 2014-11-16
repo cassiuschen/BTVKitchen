6b220427624a56ca9cee66ea28f355ca5156c776
夏 子皓 <seaver@natsumatoMacBook-Air.local>
2014-11-16 17:43:36 +0800

    Initial Commit

---
+++ BTVKichen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BTVKichen
+//
+//  Created by 夏 子皓 on 14/11/16.
+//  Copyright (c) 2014年 com.seaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BTVKichen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BTVKichen
+//
+//  Created by 夏 子皓 on 14/11/16.
+//  Copyright (c) 2014年 com.seaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
